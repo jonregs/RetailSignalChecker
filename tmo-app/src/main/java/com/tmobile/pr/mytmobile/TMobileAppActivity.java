7811c35bee88887386c7d2c986dfb453539ee0c1
Regalado, Jonathan <Jonathan.Regalado7@T-Mobile.com>
2017-09-18 15:13:59 -0700

    Initial Commit

---
+++ tmo-app/src/main/java/com/tmobile/pr/mytmobile/TMobileAppActivity.java
@@ -0,0 +1,12 @@
+package com.tmobile.pr.mytmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TMobileAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
