f67323e0a4873a1bb3ee51b07ee5f6d993b87d8f
Rajendar Are <Rajendar.Are@T-Mobile.com>
2017-10-09 10:27:10 -0700

    US216448 Remove timer from splash screen.

--- TMOAndroidApp/tmoapp-presentation/src/main/java/com/tmobile/pr/mytmobile/SplashActivity.java
+++ TMOAndroidApp/tmoapp-presentation/src/main/java/com/tmobile/pr/mytmobile/SplashActivity.java
@@ -2,7 +2,6 @@ package com.tmobile.pr.mytmobile;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -14,20 +13,12 @@ import com.tmobile.pr.mytmobile.login.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static final int SPLASH_TIME_OUT = 5000;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        new Handler().postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                Intent loginIntent = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(loginIntent);
-                SplashActivity.this.finish();
-            }
-        }, SPLASH_TIME_OUT);
+        Intent loginIntent = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        SplashActivity.this.finish();
     }
 }
