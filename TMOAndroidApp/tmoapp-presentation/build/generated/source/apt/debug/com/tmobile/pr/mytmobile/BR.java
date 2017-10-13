335f9e553f9ee22911013381ef8a3d96911e5c55
Srikanth Roopa <Srikanth.Roopa1@T-Mobile.com>
2017-10-13 13:46:47 -0700

    Revert "Update from the remote repo"
    This reverts commit c372b3daaf5ec1f67ea151826991c494cdcb8712.

--- TMOAndroidApp/tmoapp-presentation/build/generated/source/apt/debug/com/tmobile/pr/mytmobile/BR.java
+++
@@ -1,7 +0,0 @@
-package com.tmobile.pr.mytmobile;
-
-public class BR {
-        public static final int _all = 0;
-        public static final int isLoading = 1;
-        public static final int userModel = 2;
-}
--- TMOAndroidApp/tmoapp-presentation/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="510ba5d1-5436-4c59-967f-d2e106eb1592")
-public class DataBindingInfo {}
